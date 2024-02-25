9c7893a2c05903a439cefa15b29405351faa3423
Pislyakov Oleg <olegpislyakov@mail.ru>
2024-02-25 04:12:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,132 @@
+function movePaginationToTop() {
+    // Находим точки пагинации внутри .owl-carousel
+    let pagination = $('.offer__slider.owl-carousel .owl-dots');
+
+    // Проверяем, найдены ли точки пагинации
+    if (pagination.length) {
+        // Перемещаем точки пагинации в конец тела документа
+        $('.offer .container').append(pagination);
+    }
+}
+
+$(document).ready(function () {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        var header = $('header');
+    
+        if (scroll >=  100) {
+            header.addClass('scrolled');
+            $('.header__social').slideUp(300)
+            $('.header__address').slideUp(300)
+        } else {
+            header.removeClass('scrolled')
+            $('.header__social').slideDown(600)
+            $('.header__address').slideDown(600)
+        }
+    });
+    
+    $('.has-submenu').hover(function(e){
+        $(this).children('.submenu').slideToggle(200);
+    })
+
+    //Карусель в офферном блоке
+
+    $(".offer__slider.owl-carousel").owlCarousel({
+        items:  1, // Количество элементов в слайде
+        loop: true, // Включение бесконечного цикла
+        margin:  0, // Отступ между элементами
+        autoplay: true, // Автоматическая прокрутка
+        autoplayTimeout:  5000, // Время между автопрокруткой в миллисекундах
+        nav: false, // Включение навигации
+        dots: true, // Включение точек для перехода к слайду
+        mouseDrag: true,
+        touchDrag: true,
+    });
+    movePaginationToTop(); //Перемещение точек пагинации выше по иерархии DOM
+
+    //Карусель в блоке "О нас"
+
+    $(".about__slider.owl-carousel").owlCarousel({
+        items:  1, // Количество элементов в слайде
+        loop: true, // Включение бесконечного цикла
+        margin:  50, // Отступ между элементами
+        autoplay: true, // Автоматическая прокрутка
+        autoplayTimeout:  5000, // Время между автопрокруткой в миллисекундах
+        nav: false, // Включение навигации
+        dots: false, // Включение точек для перехода к слайду
+        mouseDrag: true,
+        touchDrag: true,
+        smartSpeed: 500,
+    });
+    
+    //Карусель в блоке "События"
+
+    $(".events__slider.owl-carousel").owlCarousel({
+        items:  3, // Количество элементов в слайде
+        loop: true, // Включение бесконечного цикла
+        margin:  20, // Отступ между элементами
+        autoplay: true, // Автоматическая прокрутка
+        autoplayTimeout:  5000, // Время между автопрокруткой в миллисекундах
+        nav: true, // Включение навигации
+        navText: ["<", ">"],
+        dots: false, // Включение точек для перехода к слайду
+        mouseDrag: true,
+        touchDrag: true,
+        smartSpeed: 500,
+    });
+
+    //Увеличение второго слайда в блоке "События"
+
+    $('.events__slider .owl-item.active').eq(1).addClass('sized');
+    $(".events__slider.owl-carousel").on("changed.owl.carousel", function(event) {
+        $('.events__slider .owl-item').each(function(index){
+            $(this).removeClass('sized');
+        })
+        $('.events__slider .owl-item').eq(event.item.index + 1).addClass('sized');
+    });
+
+    $('.schedule__item').each(function(index){
+        let calcIndex = index %  12; 
+
+            switch (calcIndex) {
+                case 0:
+                    $(this).children(".schedule__hour").addClass("orange")
+                    break;
+                case 1:
+                    $(this).children(".schedule__hour").addClass("red")
+                    break;
+                case 2:
+                    $(this).children(".schedule__hour").addClass("yellow")
+                    break;
+                case 3:
+                    $(this).children(".schedule__hour").addClass("violet")
+                    break;
+                case 4:
+                    $(this).children(".schedule__hour").addClass("green")
+                    break;
+                case 5:
+                    $(this).children(".schedule__hour").addClass("blue")
+                    break;
+                case 6:
+                    $(this).children(".schedule__hour").addClass("blue")
+                    break;
+                case 7:
+                    $(this).children(".schedule__hour").addClass("green")
+                    break;
+                case 8:
+                    $(this).children(".schedule__hour").addClass("violet")
+                    break;
+                case 9:
+                    $(this).children(".schedule__hour").addClass("yellow")
+                    break;
+                case 10:
+                    $(this).children(".schedule__hour").addClass("red")
+                    break;
+                case 11:
+                    $(this).children(".schedule__hour").addClass("orange")
+                    break;
+                default:
+                    console.log("none");
+            }
+    });
+});
